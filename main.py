7206b41ce0d289663f49eca8203e9cd7e9fc2af8
John Dilworth <john.dilworth@gmail.com>
2016-05-17 21:49:21 -0600

    format fix

--- main.py
+++ main.py
@@ -119,12 +119,13 @@ while True:
 
   if (buttons & cwiid.BTN_2):
     print 'Button 2 pressed'
-    time.sleep(button_delay)
+	time.sleep(button_delay)
 
   if (buttons & cwiid.BTN_A):
 	print 'Button A pressed'
+	time.sleep(button_delay)
 	player = OMXPlayer('videos/stag.mp4', '-o local')
-    time.sleep(button_delay)
+
 
   if (buttons & cwiid.BTN_B):
     print 'Button B pressed'
