ab36e059ba0493ee709235a39c1ada3a70334e48
John Dilworth <john.dilworth@gmail.com>
2016-05-17 22:25:39 -0600

    commented more properties out

--- pyomxplayer.py
+++ pyomxplayer.py
@@ -33,11 +33,11 @@ class OMXPlayer(object):
         # (self.audio['streams'], self.video['streams'],
         #  self.chapters, self.subtitles) = [int(x) for x in file_props]
         # Get video properties
-        video_props = self._VIDEOPROP_REXP.match(self._process.readline()).groups()
-        self.video['decoder'] = video_props[0]
-        self.video['dimensions'] = tuple(int(x) for x in video_props[1:3])
-        self.video['profile'] = int(video_props[3])
-        self.video['fps'] = float(video_props[4])
+        # # video_props = self._VIDEOPROP_REXP.match(self._process.readline()).groups()
+        # self.video['decoder'] = video_props[0]
+        # self.video['dimensions'] = tuple(int(x) for x in video_props[1:3])
+        # self.video['profile'] = int(video_props[3])
+        # self.video['fps'] = float(video_props[4])
         # Get audio properties
         audio_props = self._AUDIOPROP_REXP.match(self._process.readline()).groups()
         self.audio['decoder'] = audio_props[0]
